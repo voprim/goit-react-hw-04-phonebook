511ba89464cb3ef49df21064d77ab623b1b1b833
Vladimir Korolkov <voprim@gmail.com>
2022-11-25 21:31:24 +0200

    LocalStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,13 +8,9 @@ import {ContactList} from './ContactList/ContactList';
 
 export function App() {
   const [contacts, setContacts] = useState(
-    [
-      { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
-      { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
-      { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
-      { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
-    ]
+    () => JSON.parse(window.localStorage.getItem('contacts')) ?? []
   );
+
   const [filter, setFilter] = useState('');
   
   useEffect(() => {
